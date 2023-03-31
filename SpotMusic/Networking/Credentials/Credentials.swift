f1846dc9eee246944bf165445b9565c258b5e3d8
Julian Marzabal <julianmarzabal@gmail.com>
2023-03-30 21:42:19 -0300

    Enviroment variables created

---
+++ SpotMusic/Networking/Credentials/Credentials.swift
@@ -0,0 +1,26 @@
+//
+//  Credentials.swift
+//  SpotMusic
+//
+//  Created by Julian Marzabal on 30/03/2023.
+//
+
+import Foundation
+
+struct SpotMusicCredentials {
+    
+    static let clientID: String = ProcessInfo.processInfo.environment["client_ID"] ?? "Default"
+    static let clientSecretID: String = ProcessInfo.processInfo.environment["client_SecretID"] ?? "Default"
+    private static func base64EncodedCredentials() -> String {
+            let credentialsString = "\(clientID):\(clientSecretID)"
+            guard let credentialsData = credentialsString.data(using: .utf8) else {
+                return "Default"
+            }
+            return credentialsData.base64EncodedString()
+        }
+        
+        static let authorization: String = {
+            let base64Credentials = base64EncodedCredentials()
+            return "Basic \(base64Credentials)"
+        }()
+}
